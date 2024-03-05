542b81864797a37d60b0ee3aa112287c4db6e1f1
Shifana28farveen <shifanafarveen.sightspectrum@gmail.com>
2024-03-04 22:54:52 -0800

    Javascript

---
+++ Javascript/index.js
@@ -0,0 +1,88 @@
+// console.log("I like pizza");
+// console.log("It's really good!");
+
+// window.alert("I really love pizza!");
+
+// let firstName = "Bro";
+// let age = 21;
+// let student = true;
+
+// console.log("Hello", firstName);
+// console.log("you are", age,"years old");
+// console.log("Enrolled:", student);
+
+// document.getElementById("p1").innerHTML = "Hello" + firstName;
+// document.getElementById("p2").innerHTML = "You are" + "years old";
+// document.getElementById("p3").innerHTML = "Enrolled" +  student;
+
+//arithmetic expressions
+
+// let price = 10;
+
+// price1= price+1;
+// price2 = price-1;
+// price3= price * 2;
+// price4= price / 2;
+
+// let extraprice = price % 2;
+
+// console.log(extraprice);
+
+// let result = 1+2*(3+4);
+// console.log(result);
+
+// let result1 = (1+4) * (3+6);
+// console.log(result1);
+
+// let a = 190;
+// let b = 200;
+
+// let c = a + b ;
+
+// console.log (c);
+
+// let username;
+
+// document.getElementById("myButton").onclick = function(){
+//     username = document.getElementById("myText").Value;
+//     console.log(username);
+//     document.getElementById("myLabel").innerHTML = "Hello" + username;
+// }
+
+// let x;
+// let y;
+// let z;
+
+// x = Number("pizza");
+// y = string("3.14");
+// z =  boolean ("pizza");
+
+// console.log(x,typeof x);
+// console.log(y,  typeof y);
+// console.log(z,typeof z);
+
+// let age= window.prompt("How old are you?");
+// age += 1;
+// console.log("Happy Birthday! you are", age, "years old");
+
+//string methods 
+
+let username = "Javascript";
+let phoneNumber = "123-456-7890";
+
+console.log(username);
+
+console.log(username.length);
+console.log(username.charAt(3));
+console.log(username.indexOf("s"));
+console.log(username.lastIndexOf("p"));
+console.log(username.trim());
+console.log(username.toUpperCase());
+console.log(username.toLowerCase());
+
+phoneNumber = phoneNumber.replaceAll("-", "");
+
+console.log(phoneNumber);
+
+
+
